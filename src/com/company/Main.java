88d94a4833b9605bcdb985b73e0a9ab821121de6
Trevor Antram <trevor.antram@outlook.com>
2016-06-03 21:39:36 -0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.Encryptions.normal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        com.company.Encryptions.normal normal = new normal();
+
+
+
+
+    }
+}
+//hahahahh this is a test
---
+++ src/com/company/basic.java
@@ -0,0 +1,42 @@
+package com.company;
+
+/**
+ * Package com.company
+ * Created by trevor2076 on 6/1/16/Jun/2016}.
+ * <p>
+ * The following code may not be used without the explicit permission of trevor2076
+ */
+public class basic {
+
+
+    public String encrypt(String message1,int charReturn) {
+        String message = message1;
+
+
+        char arr[] = message.toCharArray(); // convert the String object to array of char
+        String result = String.valueOf(arr[charReturn]);
+
+
+
+
+
+
+
+    return result;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
